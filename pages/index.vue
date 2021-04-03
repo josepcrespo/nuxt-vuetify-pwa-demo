3f302033578498d791ba1bcfddc337542484c27f
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2021-04-03 20:56:48 +0100

    Using the brand new `UsersDataTable` component on the Users page.

--- pages/index.vue
+++ pages/index.vue
@@ -12,138 +12,41 @@
             <v-icon left>
               mdi-database-refresh
             </v-icon>
-            Get new data
+            Get new users
           </v-btn>
         </template>
-        <span>Discard current data and, get new random data from randomuser.me API</span>
+        <span>Discard current users and, get new random users from randomuser.me API</span>
       </v-tooltip>
     </v-col>
     <v-col cols="12">
-      <v-data-table
-        :headers="usersHeaders"
-        :items="$store.state.users.list"
-        :loading="users.loading.state"
-        :loading-text="users.loading.text"
-        elevation="1"
-      >
-        <template v-slot:item.dob.date="{ item }">
-          {{ new Date(item.dob.date).toDateString() }}
-        </template>
-        <template v-slot:item.registered.date="{ item }">
-          {{ new Date(item.registered.date).toDateString() }}
-        </template>
-        <template v-slot:item.details="{ item }">
-          <nuxt-link :to="{ name: 'user-details', params: { user: item }}">
-            <v-icon>
-              mdi-account-details
-            </v-icon>
-          </nuxt-link>
-        </template>
-        <template v-slot:body.append>
-          <tr>
-            <td>
-              <v-select
-                v-model="filters.gender"
-                :items="['', 'Male', 'Female']"
-                label="Gender"
-              />
-            </td>
-            <td colspan="2" />
-            <td>
-              <v-text-field
-                v-model="filters.nationality"
-                type="string"
-                title="ISO 3166-1 alpha-2"
-                label="Nationality"
-              />
-            </td>
-            <td>
-              <v-text-field
-                v-model="filters.age"
-                type="number"
-                label="Age (equal/older)"
-              />
-            </td>
-          </tr>
-        </template>
-      </v-data-table>
+      <users-data-table
+        :loading-state="users.loading"
+        :users-list="usersList"
+        :with-filters="true"
+      />
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: 'AppUsersList',
+  name: 'UsersListPage',
   data () {
     return {
-      filters: {
-        age: null,
-        gender: '',
-        nationality: ''
-      },
       users: {
-        loading: {
-          state: true,
-          text: 'Loading favorites... Please wait'
-        },
+        loading: false,
         quantity: 100
       }
     }
   },
   computed: {
-    usersHeaders () {
-      return [{
-        text: 'Gender',
-        value: 'gender',
-        filter: (value) => {
-          if (!this.filters.gender) {
-            return true
-          } else {
-            return value === this.filters.gender.toLowerCase()
-          }
-        }
-      }, {
-        text: 'Name',
-        value: 'name.first'
-      }, {
-        text: 'Email',
-        value: 'email'
-      }, {
-        text: 'Nationality',
-        value: 'nat',
-        filter: (value) => {
-          if (!this.filters.nationality) {
-            return true
-          } else {
-            return value === this.filters.nationality.toUpperCase()
-          }
-        }
-      }, {
-        text: 'Date of Birth',
-        value: 'dob.date',
-        filter: (value) => {
-          if (!this.filters.age) {
-            return true
-          } else {
-            const diffMs = Date.now() - new Date(value).getTime()
-            const ageDate = new Date(diffMs)
-
-            return Math.abs(ageDate.getUTCFullYear() - 1970) >= this.filters.age
-          }
-        }
-      }, {
-        text: 'Registration Date',
-        value: 'registered.date'
-      }, {
-        text: 'Details',
-        value: 'details',
-        sortable: false
-      }]
+    usersList () {
+      return this.$store.state.users.list
     }
   },
-  created () {
-    if (this.$store.state.users.list.length) {
-      this.users.loading.state = false
+  mounted () {
+    if (this.usersList.length) {
+      this.users.loading = false
     } else {
       this.getUsers(this.users.quantity)
     }
@@ -163,7 +66,7 @@ export default {
         }
       ).then((response) => {
         this.$store.commit('users/set', response.results)
-        this.users.loading.state = false
+        this.users.loading = false
       }).catch((error) => {
         // eslint-disable-next-line no-console
         console.warn('Something went wrong: %o', error)
@@ -172,18 +75,3 @@ export default {
   }
 }
 </script>
-
-<style>
-a {
-  text-decoration: none;
-}
-
-th:nth-of-type(5),
-th:nth-of-type(6) {
-  min-width: 170px;
-}
-
-th:nth-of-type(7) {
-  width: 70px;
-}
-</style>
