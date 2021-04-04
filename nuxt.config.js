c5692b30dd1f745f4e5fb4718e5ce01561c6da30
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2021-04-04 22:20:26 +0100

    The base URL for the Favorites microservice has been set as an `env` variable.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  env: {
+    favoritesApiBaseUrl: process.env.FAVORITES_API_BASE_URL || 'http://localhost:3030'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - nuxt-vuetify-pwa-demo',
