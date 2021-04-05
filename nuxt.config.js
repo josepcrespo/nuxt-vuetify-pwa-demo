76bb6d149908f174488d690dfc403893e7385262
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2021-04-05 05:16:43 +0100

    The `npm run build` command needs this bugfix to compile without erros.
    Fix this error:
      Cannot find module 'vuetify/es5/util/colors' in /nuxt.config.js
    
    More info:
    https://github.com/nuxt-community/vuetify-module/issues/101#issuecomment-518577403

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   env: {
     favoritesApiBaseUrl: process.env.FAVORITES_API_BASE_URL || 'http://localhost:3030'
@@ -66,20 +64,16 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    /**
+     * As described here:
+     * https://github.com/nuxt-community/vuetify-module/issues/101#issuecomment-518577403
+     *
+     * Defining options like this will make the Vuetify colors will be
+     * bundled by Webpack at build step and, should fixes the error:
+     *
+     * Cannot find module 'vuetify/es5/util/colors' in /nuxt.config.js
+     */
+    optionsPath: './vuetify.options.js'
   },
 
   // https://color-mode.nuxtjs.org/#configuration
